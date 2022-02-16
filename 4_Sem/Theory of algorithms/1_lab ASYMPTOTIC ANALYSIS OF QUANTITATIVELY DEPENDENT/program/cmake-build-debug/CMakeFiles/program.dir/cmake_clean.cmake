19bd8273d51c30d432da4ff380dfd4d1ec959fe7
Danil <danilkhromdewil@gmail.com>
2022-02-16 22:44:34 +0300

    working with the lab

---
+++ 4_Sem/Theory of algorithms/1_lab ASYMPTOTIC ANALYSIS OF QUANTITATIVELY DEPENDENT/program/cmake-build-debug/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/main.c.obj"
+  "libprogram.dll.a"
+  "program.exe"
+  "program.exe.manifest"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
